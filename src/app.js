c496dea6e1af1ff980b9195ffd47138bd21ca47a
Mathias Iden <mathiasiden@me.com>
2016-05-27 18:41:32 +0200

    Did something

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var starwars = require('starwars')
-app.use(bodyParser.json())
-var Message = require('./Controllers/Message')
+const restify = require('restify')
+const starwars = require('starwars')
+const app = restify.createServer();
+const Message = require('./Controllers/Message')
 
-app.get('/', function(req, res){
-	res.status(200).send("MOM, IM ON A CLOUD!")
-})
+app.use(restify.acceptParser(app.acceptable));
+app.use(restify.bodyParser())
+app.use(restify.queryParser())
+app.use(restify.gzipResponse());
 
-app.get('/webhooks', function(req, res){
+app.get('/', (req, res) => res.status(200, "MOM, IM ON A CLOUD!"))
+
+app.get('/webhooks', (req, res) => {
 	if(req.query['hub.verify_token'] === "muchsecret"){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong validation token')
 })
 
-app.post('/webhooks', (req, res) => {
-		let event = Message.parseMessage(req);
-		if(event.sender && event.text){
-			Message.sendMessageTo(event.sender, starwars())
-			.then(() => {
-				res.sendStatus(200)
-			})
-			.catch((error) => {
-			})
-		} else {
-			res.sendStatus(200)
-		}
+app.post('/webhooks', (req, res, next) => {
+	let event = Message.parseMessage(req);
+	if(event.sender && event.text){
+		const randomStarwarsQuote = starwars()
+		Message.sendMessageTo(event.sender, randomStarwarsQuote)
+		.then(() => {
+			res.send(200, null)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+	} else {
+		res.send(200, null)
+	}
 })
 
 module.exports = {
